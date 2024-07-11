116ae36a25d7c2c1f9b6a9125b73ba0f4d222ad4
Rabah Djebbes <39097998+rabahdj2002@users.noreply.github.com>
2024-07-11 23:16:30 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='wilaya',  # Replace with your package name
+    name='wilayati',  # Replace with your package name
     version='1.0.0',  # Initial release version
     packages=find_packages(),  # Automatically find packages in the current directory
     install_requires=[],  # List of dependencies
@@ -10,7 +10,7 @@ setup(
     description='This Python module provides functionality for searching and retrieving information about Algerian Wilayas (provinces) and their respective Communes (municipalities).',  # Short description of the package
     long_description=open('README.md').read(),  # Read long description from README file
     long_description_content_type='text/markdown',  # Type of long description content
-    url='https://github.com/rabahdj2002/wilaya',  # URL of the project (GitHub, etc.)
+    url='https://github.com/rabahdj2002/Wilayas-of-Algeria',  # URL of the project (GitHub, etc.)
     classifiers=[  # List of classifiers that describe the package
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
